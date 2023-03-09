46c56c07e7651e747a21e7099078aa9bc14c75b5
Polgár Attila (SZF_B_2021_2023) <polgar.attila@diak.szamalk-szalezi.hu>
2023-03-09 13:52:33 +0100

    mentéssel

--- Aranyhal.java
+++ Aranyhal.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 public class Aranyhal {
     private static int KIVANSAGOKSZAMA = 3;
-
-    
     private int kor;
     private Map<String, Integer> kivansagok = new HashMap<String, Integer>();
     private String kifogta;
@@ -18,6 +16,31 @@ public class Aranyhal {
     public Aranyhal(int kor, String kifogta) {
         this.kor = kor;
         this.kifogta = kifogta;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKifogta(String kifogta) {
+        this.kifogta = kifogta;
+    }
+    
+    public int kivansagokSzama(String kifogta){
+        return 0;
+    }
+    
+    public String[] kikkelTalalkozott(){
+        return null;
+    }
+    
+    public boolean kivansagotTeljesit(String kivansag){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Aranyhal{" + "kor=" + kor + ", kivansagok=" + kivansagok + ", kifogta=" + kifogta + '}';
     }
     
     
